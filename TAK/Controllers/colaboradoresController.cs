ee085754f4d29a20aa8ac1d1a5d214cba3077bbc
mauriciohurtado99 <maurohurtadogt183@gmail.com>
2023-03-29 14:03:53 -0600

    first commit23

---
+++ TAK/Controllers/colaboradoresController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TAK.Models;
+
+namespace TAK.Controllers
+{
+    public class colaboradoresController : Controller
+    {
+        private TakEntities db = new TakEntities();
+
+        // GET: colaboradores
+        public ActionResult Index()
+        {
+            return View(db.colaboradores.ToList());
+        }
+
+        // GET: colaboradores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            colaboradores colaboradores = db.colaboradores.Find(id);
+            if (colaboradores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(colaboradores);
+        }
+
+        // GET: colaboradores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: colaboradores/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombres,apellidos,fecha_nacimiento,estado_civil,grado_academico,direccion")] colaboradores colaboradores)
+        {
+            if (ModelState.IsValid)
+            {
+                db.colaboradores.Add(colaboradores);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(colaboradores);
+        }
+
+        // GET: colaboradores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            colaboradores colaboradores = db.colaboradores.Find(id);
+            if (colaboradores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(colaboradores);
+        }
+
+        // POST: colaboradores/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombres,apellidos,fecha_nacimiento,estado_civil,grado_academico,direccion")] colaboradores colaboradores)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(colaboradores).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(colaboradores);
+        }
+
+        // GET: colaboradores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            colaboradores colaboradores = db.colaboradores.Find(id);
+            if (colaboradores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(colaboradores);
+        }
+
+        // POST: colaboradores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            colaboradores colaboradores = db.colaboradores.Find(id);
+            db.colaboradores.Remove(colaboradores);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult GetJson(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            colaboradores colaboradores = db.colaboradores.Find(id);
+
+            if (colaboradores == null)
+            {
+                return HttpNotFound();
+            }
+
+            return Json(colaboradores, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ TAK/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TAK.MvcApplication" Language="C#" %>
